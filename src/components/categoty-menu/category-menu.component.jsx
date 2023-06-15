3296d0b642c139fa15527e6f9be89b90e41154f5
Florent-NOEL <121178866+Florent-NOEL@users.noreply.github.com>
2023-06-15 12:20:17 +0200

    put categories back to app.js

--- src/components/categoty-menu/category-menu.component.jsx
+++ src/components/categoty-menu/category-menu.component.jsx
@@ -1,35 +1,8 @@
 import './category-menu.styles.scss';
 import CategoryItem from '../category-item/category-item.components';
 
-const categories = [
-    {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ]
 
-const CategoryMenu = () => (
+const CategoryMenu = ({categories}) => (
     <div className="categories-container">
     {categories.map((category) => (
       <CategoryItem key={categories.id} category={category}/>

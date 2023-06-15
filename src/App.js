1ed7c8d8b55e50d0ace7f1c41e7207008bef1394
Florent-NOEL <121178866+Florent-NOEL@users.noreply.github.com>
2023-06-15 14:20:10 +0200

    save change

--- src/App.js
+++ src/App.js
@@ -1,39 +1,30 @@
 
+import { Routes, Route, Outlet } from "react-router-dom";
 import "./categories.styles.scss";
-import CategoryMenu from "./components/categoty-menu/category-menu.component";
+import Home from "./components/routes/home/home.component";
 
-const categories = [
-  {
-    "id": 1,
-    "title": "hats",
-    "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-  },
-  {
-    "id": 2,
-    "title": "jackets",
-    "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-  },
-  {
-    "id": 3,
-    "title": "sneakers",
-    "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-  },
-  {
-    "id": 4,
-    "title": "womens",
-    "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-  },
-  {
-    "id": 5,
-    "title": "mens",
-    "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-  }
-]
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>
+        I'm the nav bar
+        </h1>
+      </div>
+      <Outlet />
+    </div>
+  )
+}
 
 const App = () => {
   
   return (
-    <CategoryMenu categories={categories} />
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+    
   );
 }
 
